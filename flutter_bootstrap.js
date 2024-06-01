4ed406968961b601f0593661daec5d467cd0a453
Mark Cabalona <markcabalona58@gmail.com>
2024-06-01 23:29:16 +0800

    feature: add email verification feature

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f6344b75dcf861d8bf1f1322780b8811f982e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "696783665"
+    serviceWorkerVersion: "1231148135"
   }
 });
